b5c1d02a78048beea76e9c092dca227ba2e18672
Nikhil Singh Gautam <nick9124>
2020-08-10 05:21:26 +0530

    Updates

---
+++ precache-manifest.3e9cdbbf4e5333695c4fa96595642463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a92995878440f700d669bbdff37ded",
+    "url": "/img-search-app/index.html"
+  },
+  {
+    "revision": "c7b946fcec02b6c970fe",
+    "url": "/img-search-app/static/css/main.310e5213.chunk.css"
+  },
+  {
+    "revision": "52126719179e2b5771ec",
+    "url": "/img-search-app/static/js/2.0d8d10a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/img-search-app/static/js/2.0d8d10a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b946fcec02b6c970fe",
+    "url": "/img-search-app/static/js/main.6b6e6e6d.chunk.js"
+  },
+  {
+    "revision": "08284ee666ca0e30cdaf",
+    "url": "/img-search-app/static/js/runtime-main.cf9b4425.js"
+  }
+]);
