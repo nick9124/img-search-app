b713243a5f5070c132cc7f5bba19d9649b9e4e11
N S Gautam <58429798+nick9124@users.noreply.github.com>
2022-10-16 14:35:07 +0530

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID _________',
+    Authorization: 'Client-ID XUttPyiXJRqifd2TDCZm1aQxZ8mWZi2d4V0lpEzKmoM',
   },
 });
